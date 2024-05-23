d9dbc905ab0c07b38f92934d8b4e39deb395ad5f
Kevin Farhan H <kevinfarhan95@gmail.com>
2024-05-23 10:13:20 +0700

    kevin, features/create-transaction-table

--- resources/views/components/organisms/sidebar.blade.php
+++ resources/views/components/organisms/sidebar.blade.php
@@ -1,17 +1,20 @@
-<div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-blue-custom">
-    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-5 d-none d-sm-inline">Mikhayla Kain</span>
-        </a>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-            <li class="nav-item">
-                <a href="{{ route('dashboard') }}" class="nav-link align-middle px-0">
-                    <i class="fs-4 bi-house text-white"></i> <span class="ms-1 d-none d-sm-inline text-white">Home</span>
-                </a>
+<div class="col-auto col-md-2 col-xl-2 bg-blue-custom">
+    <div class="d-flex flex-column align-items-center align-items-sm-start pt-2 text-white min-vh-100">
+        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100"
+            id="menu">
+            <li class="nav-item w-100">
+                <div class="{{ Route::currentRouteName() == 'dashboard' ? 'active' : '' }} font-weight-bold ps-3">
+                    <a href="{{ route('dashboard') }}" class="nav-link align-middle px-0">
+                        <i class="fs-4 bi-house text-white me-3"></i> <span class="d-none d-sm-inline text-white fs-5 ">Home</span>
+                    </a>
+                </div>
             </li>
-            <li>
-                <a href="{{ route('transaksi.index') }}" class="nav-link px-0 align-middle">
-                    <i class="fs-4 bi-table text-white"></i> <span class="ms-1 d-none d-sm-inline text-white">Data Transaksi</span></a>
+            <li class="nav-item w-100">
+                <div class="{{ Route::currentRouteName() == 'transaksi.index' ? 'active' : '' }} font-weight-bold ps-3">
+                    <a href="{{ route('transaksi.index') }}" class="nav-link px-0 align-middle">
+                        <i class="fs-4 bi-table text-white me-3"></i> <span class="d-none d-sm-inline text-white fs-5">Data Transaksi</span>
+                    </a>
+                </div>
             </li>
         </ul>
         <hr>
